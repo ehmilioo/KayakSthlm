fe70a31a4b365328d8e24fc4f34140dcfd7e15d4
emilkrslak <krslak_emil@hotmail.com>
2021-04-28 10:36:49 +0200

    Navigation Bar
    Navigation bar som använder sig utav openPage(context) för att ta sig runt i appen

--- kayak_sthlm/lib/screens/home/home.dart
+++ kayak_sthlm/lib/screens/home/home.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:kayak_sthlm/services/auth.dart';
+import 'package:kayak_sthlm/screens/authenticate/reset_pass.dart';
+
 
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
+
+
+  void openPage(BuildContext context) {
+    Navigator.push(context,
+      MaterialPageRoute(builder: (context) {
+        return ResetPass();
+      })
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +34,48 @@ class Home extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Text('Welcome ', style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 40.0),
+        child: FloatingActionButton(
+          onPressed: () {
+            openPage(context);
+          },
+          tooltip: 'Start',
+          child: Icon(Icons.play_arrow)
+        ),
+      ),
+        
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.wb_sunny_outlined),
+                iconSize: 30,
+                onPressed: (){},
+              ),
+              IconButton(
+                icon: Icon(Icons.person_outline_outlined),
+                iconSize: 30,
+                onPressed: (){},
+              ),
+              IconButton(
+                icon: Icon(Icons.sticky_note_2_outlined),
+                iconSize: 30,
+                onPressed: (){},
+              ),
+              IconButton(
+                icon: Icon(Icons.settings),
+                iconSize: 30,
+                onPressed: (){},
+              ),
           ],
         ),
       ),
