73fc3b923ac734f4558ba7507ec084a4ea3fae8b
emilkrslak <krslak_emil@hotmail.com>
2021-04-30 11:26:18 +0200

    Home Google Maps
    Implementerade Google maps karta i Hemskärmen med kompass

--- kayak_sthlm/lib/screens/home/home.dart
+++ kayak_sthlm/lib/screens/home/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'dart:async';
 import 'package:kayak_sthlm/services/auth.dart';
 import 'package:kayak_sthlm/services/database.dart';
 import 'package:kayak_sthlm/screens/authenticate/reset_pass.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget  {
+  @override
+  State<Home> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<Home> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
   final AuthService _auth = AuthService();
   final Database db = new Database();
 
@@ -18,37 +27,40 @@ class Home extends StatelessWidget {
     db.getMap();
   }
 
+  bool terrain = false;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  CameraPosition _userPos = CameraPosition(
+    bearing: 300.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    zoom: 19.151926040649414
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Welcome back'),
-        backgroundColor: Colors.red[600],
-        elevation: 0.0,
-        actions: <Widget>[
-          ElevatedButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              style: ElevatedButton.styleFrom(primary: Colors.deepOrange[600]),
-              icon: Icon(Icons.lock_open),
-              label: Text('Logga ut'))
-        ],
-      ),
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          children: <Widget>[],
-        ),
+
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: terrain ? MapType.hybrid : MapType.normal,
+        zoomControlsEnabled: false,
+        compassEnabled: true,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 30.0),
         child: FloatingActionButton(
             onPressed: () {
-              openPage(context);
+              setState(() =>!terrain);
+              print(terrain);
             },
             tooltip: 'Start',
             backgroundColor: Colors.green[200],
@@ -98,5 +110,10 @@ class Home extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_userPos));
   }
 }
