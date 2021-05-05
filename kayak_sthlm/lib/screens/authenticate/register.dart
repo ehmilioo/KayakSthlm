d5f95ee46f5e54a5059af29db1f0ec1669c3f55e
axelwegner <wegaxe@gmail.com>
2021-05-05 14:51:54 +0200

    Front end login
    login, resetpass, resetpassok

--- kayak_sthlm/lib/screens/authenticate/register.dart
+++ kayak_sthlm/lib/screens/authenticate/register.dart
@@ -4,7 +4,6 @@ import 'package:flutter_material_pickers/flutter_material_pickers.dart';
 import 'package:kayak_sthlm/services/auth.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
   Register({this.toggleView});
 
@@ -13,7 +12,6 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
@@ -22,7 +20,7 @@ class _RegisterState extends State<Register> {
   String email = '';
   String password = '';
   String error = '';
-  
+
   //Imported package vars
   var age = 'Age';
   var selectedExperienceLevel = "Skill level";
@@ -35,12 +33,8 @@ class _RegisterState extends State<Register> {
     'Specialist',
     'Expert',
   ];
-  List<String> genders = <String>[
-    'Male',
-    'Female',
-    'Other'
-  ];
-  
+  List<String> genders = <String>['Male', 'Female', 'Other'];
+
   //Bool
   bool hidePassword = true; //Obscure passwords with buttons and icons
   bool validatedInput = false;
@@ -53,158 +47,171 @@ class _RegisterState extends State<Register> {
     return (!regex.hasMatch(value)) ? true : false;
   }
 
-  bool validatePassword(String value){
-    Pattern pattern = 
-        r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$';
+  bool validatePassword(String value) {
+    Pattern pattern = r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$';
     RegExp regex = new RegExp(pattern);
-    return(!regex.hasMatch(value)) ? true : false;
+    return (!regex.hasMatch(value)) ? true : false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height:50.0),
-              Text.rich(
-                TextSpan(
-                  text: 'Create',
-                  style: TextStyle(fontSize: 30),
-                  children: <TextSpan>[
-                    TextSpan(text: '\n Account.', style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              SizedBox(height:20.0),
-              TextFormField(
-                validator: (val) => val.length < 3 ? 'Användarnamn för kort' : null,
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                  suffixIcon : IconButton(
-                    onPressed: (){},
-                    icon: Icon(validatedInput ? Icons.check : null),
+        backgroundColor: Color.fromRGBO(242, 248, 255, 1),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/bakgrund.png"),
+            fit: BoxFit.cover,
+          )),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 60.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 180),
+                  Text(
+                    'Create Account',
+                    style:
+                        TextStyle(fontFamily: 'HammersmithOne', fontSize: 35),
                   ),
-                ),
-                onChanged: (val) {
-                  setState(() => username = val);
-                }
-              ),
-              SizedBox(height:20.0),
-              TextFormField(
-                validator: (val) => validateEmail(val) ? 'Ange en giltig e-mail' : null,
-                decoration: InputDecoration(
-                  hintText: 'E-mail'
-                ),
-                onChanged: (val) {
-                  setState(() => email = val);
-                }
-              ),
-              SizedBox(height:20.0),
-              TextFormField(
-                validator: (val) => validatePassword(val) ? 'Minst 8 tecken och minst en siffra' : null,
-                obscureText: hidePassword,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() => hidePassword = !hidePassword);
-                    },
-                    icon: Icon(hidePassword ? Icons.remove_red_eye : Icons.remove_red_eye_outlined ),
-                  )
-                ),
-                onChanged: (val) {
-                  setState(() => password = val);
-                }
-              ),
-              SizedBox(height:20.0),
+                  SizedBox(height: 110),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxHeight: 82, minHeight: 82),
+                    child: TextFormField(
+                        style: TextStyle(
+                            fontFamily: 'HammersmithOne', fontSize: 18),
+                        validator: (val) =>
+                            val.length < 3 ? 'Användarnamn för kort' : null,
+                        decoration: InputDecoration(
+                            labelText: 'Username',
+                            contentPadding: EdgeInsets.only(top: 20),
+                            labelStyle: TextStyle(
+                                color: Color.fromRGBO(136, 134, 134, 1))),
+                        onChanged: (val) {
+                          setState(() => username = val);
+                        }),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      validator: (val) =>
+                          validateEmail(val) ? 'Ange en giltig e-mail' : null,
+                      decoration: InputDecoration(hintText: 'E-mail'),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      }),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      validator: (val) => validatePassword(val)
+                          ? 'Minst 8 tecken och minst en siffra'
+                          : null,
+                      obscureText: hidePassword,
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() => hidePassword = !hidePassword);
+                            },
+                            icon: Icon(hidePassword
+                                ? Icons.remove_red_eye
+                                : Icons.remove_red_eye_outlined),
+                          )),
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      }),
+                  SizedBox(height: 20.0),
 
-              FloatingActionButton.extended(
-                    icon: Icon(Icons.keyboard_arrow_down), 
+                  FloatingActionButton.extended(
+                    icon: Icon(Icons.keyboard_arrow_down),
                     label: Text(selectedExperienceLevel),
                     backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,  
+                    foregroundColor: Colors.white,
                     onPressed: () => {
                       showMaterialRadioPicker(
                         context: context,
                         title: "Pick Your Skill Level",
                         items: experienceLevels,
-                        onChanged: (value) => setState(() => selectedExperienceLevel = value),
+                        onChanged: (value) =>
+                            setState(() => selectedExperienceLevel = value),
                       )
                     },
                   ),
-              Row(
-                children: <Widget>[
-                  FloatingActionButton.extended(
-                    icon: Icon(Icons.keyboard_arrow_down), 
-                    label: Text(selectedGender),
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,  
-                    onPressed: () => {
-                      showMaterialRadioPicker(
-                          context: context,
-                          title: "Pick Your Gender",
-                          items: genders,
-                          onChanged: (value) {
-                            setState(() => selectedGender = value);
-                          }
-                      )
-                    },
-                  ),
-                  FloatingActionButton.extended(
-                    icon: Icon(Icons.keyboard_arrow_down), 
-                    label: Text(age),
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,  
-                    onPressed: () => {
-                      showMaterialNumberPicker(
-                        context: context,
-                        title: "Pick Your Age",
-                        maxNumber: 100,
-                        minNumber: 1,
-                        onChanged: (value) {
-                          setState(() => age = value.toString());
+                  Row(
+                    children: <Widget>[
+                      FloatingActionButton.extended(
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        label: Text(selectedGender),
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                        onPressed: () => {
+                          showMaterialRadioPicker(
+                              context: context,
+                              title: "Pick Your Gender",
+                              items: genders,
+                              onChanged: (value) {
+                                setState(() => selectedGender = value);
+                              })
                         },
-                      )
+                      ),
+                      FloatingActionButton.extended(
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        label: Text(age),
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                        onPressed: () => {
+                          showMaterialNumberPicker(
+                            context: context,
+                            title: "Pick Your Age",
+                            maxNumber: 100,
+                            minNumber: 1,
+                            onChanged: (value) {
+                              setState(() => age = value.toString());
+                            },
+                          )
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 40.0),
+
+                  CupertinoButton(
+                    child: Text('Registrera'),
+                    color: CupertinoColors.activeBlue,
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        dynamic result = await _auth.registerMail(
+                            email,
+                            password,
+                            username,
+                            selectedExperienceLevel,
+                            age,
+                            selectedGender);
+                        if (result == null) {
+                          setState(() => error = 'Ett fel uppstod');
+                        }
+                      }
                     },
                   ),
+                  SizedBox(height: 12.0),
+                  //Preliminär errorhandling
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  ),
+                  CupertinoButton(
+                      child: Text('Logga in'),
+                      color: CupertinoColors.darkBackgroundGray,
+                      onPressed: () async {
+                        ElevatedButton.icon(
+                            onPressed: widget.toggleView(),
+                            icon: Icon(Icons.person),
+                            label: Text('Logga in'));
+                      }),
                 ],
               ),
-              SizedBox(height:40.0),
-              
-              CupertinoButton(
-                child: Text('Registrera'),
-                color: CupertinoColors.activeBlue,
-                onPressed: () async {
-                  if(_formKey.currentState.validate()){
-                    dynamic result = await _auth.registerMail(email, password, username, selectedExperienceLevel, age, selectedGender);
-                    if(result == null){
-                      setState(() => error = 'Ett fel uppstod');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              //Preliminär errorhandling
-              Text(
-                error, 
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-              CupertinoButton(
-                child: Text('Logga in'),
-                color: CupertinoColors.darkBackgroundGray,
-                onPressed: () async {
-                  ElevatedButton.icon(onPressed: widget.toggleView(), icon: Icon(Icons.person), label: Text('Logga in'));
-                }
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- kayak_sthlm/lib/screens/authenticate/reset_pass.dart
+++ kayak_sthlm/lib/screens/authenticate/reset_pass.dart
@@ -3,91 +3,192 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ResetPass extends StatefulWidget {
-
   ResetPass();
 
   @override
   _ResetPass createState() => _ResetPass();
 }
 
-
-
 class _ResetPass extends State<ResetPass> {
-
   final _auth = FirebaseAuth.instance;
-
+  final _formKey = GlobalKey<FormState>();
   String email = '';
+  bool _requestEnabled = false;
+  String error = '';
 
   //Check e-mail
   bool validateEmail(String value) {
-  Pattern pattern =
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  RegExp regex = new RegExp(pattern);
-  return (!regex.hasMatch(value)) ? true : false;
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    return (!regex.hasMatch(value)) ? true : false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-          children: [
-            SizedBox(height:200),
-            Text(
-              'Reset your Password',
-              style: TextStyle(fontSize: 30),
-            ),
-            TextFormField(
-              validator: (val) => validateEmail(val) ? 'Ange en giltig e-mail' : null,
-              onChanged: (val) {
-                setState(() => email = val);
-              },
-              decoration: InputDecoration(
-                labelText: 'Email',
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white
+        backgroundColor: Color.fromRGBO(242, 248, 255, 1),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/bakgrund.png"),
+            fit: BoxFit.cover,
+          )),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 60.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 180),
+                  Text(
+                    'Reset Password',
+                    style:
+                        TextStyle(fontFamily: 'HammersmithOne', fontSize: 35),
                   ),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white
+                  SizedBox(height: 110),
+                  ConstrainedBox(
+                      constraints: BoxConstraints(maxHeight: 82, minHeight: 82),
+                      child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                              fontFamily: 'HammersmithOne', fontSize: 18),
+                          validator: (val) => validateEmail(val)
+                              ? 'Ange en giltig e-post'
+                              : null,
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              contentPadding: EdgeInsets.only(top: 20),
+                              labelStyle: TextStyle(
+                                  color: Color.fromRGBO(136, 134, 134, 1))),
+                          onChanged: (val) {
+                            _requestEnabled = true;
+                            setState(() => email = val);
+                          })),
+                  SizedBox(height: 30),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: Size(281, 48),
+                        primary: _requestEnabled == true
+                            ? Colors.white
+                            : Colors.black,
+                        backgroundColor: _requestEnabled == true
+                            ? Color.fromRGBO(86, 151, 211, 1)
+                            : Color.fromRGBO(217, 221, 224, 1),
+                        shadowColor: Colors.black54,
+                        elevation: 10,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        textStyle: TextStyle(
+                            fontSize: 18, fontFamily: 'HammersmithOne'),
+                      ),
+                      child: Text('Reset Password'),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return ResetPassOk();
+                          }));
+                          // if (_formKey.currentState.validate()) {
+                          //   await _auth.sendPasswordResetEmail(email: email);
+                          //   Navigator.push(context,
+                          //       MaterialPageRoute(builder: (context) {
+                          //     return ResetPassOk();
+                          //   }));
+                          // }
+
+                          // if (_auth.sendPasswordResetEmail(email: email) ==
+                          //     null) {
+                          //   setState(() => error = 'Ett fel uppstod');
+                          // }
+                          // }
+
+                          try {
+                            await _auth.sendPasswordResetEmail(email: email);
+                          } catch (e) {
+                            print(e);
+                          }
+                        }
+                      }),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
                   ),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white
-                  ),
-                ),
+                  SizedBox(height: 188),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            primary: Color.fromRGBO(136, 134, 134, 1),
+                            textStyle: TextStyle(
+                                fontFamily: 'HammersmithOne', fontSize: 18)),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Log In')),
+                  ]),
+                ],
               ),
             ),
-            SizedBox(height:20),
-            CupertinoButton(
-              child: Text('Reset Password'),
-              color: CupertinoColors.activeBlue,
-              onPressed: () async {
-                try{
-                  await _auth.sendPasswordResetEmail(email: email);
-                }
-                catch(e){
-                  print(e);
-                }
-              }
-            ),
-            SizedBox(height:20),
-            CupertinoButton(
-              child: Text('Log in'),
-              color: CupertinoColors.activeBlue,
-              onPressed: () {
-                Navigator.pop(context);
-              }
-            ),
-          ],
           ),
-        ),
-      ),
-    );
+        ));
+  }
+}
+
+class ResetPassOk extends StatefulWidget {
+  ResetPassOk();
+
+  @override
+  _ResetPassOk createState() => _ResetPassOk();
+}
+
+class _ResetPassOk extends State<ResetPassOk> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(242, 248, 255, 1),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/bakgrund.png"),
+              fit: BoxFit.cover,
+            )),
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 60.0),
+                child: Column(children: [
+                  SizedBox(height: 180),
+                  Text(
+                    'Reset  Password',
+                    style:
+                        TextStyle(fontFamily: 'HammersmithOne', fontSize: 35),
+                  ),
+                  SizedBox(height: 110),
+                  Text(
+                    'A new password has been sent to your email.',
+                    style:
+                        TextStyle(fontFamily: 'HammersmithOne', fontSize: 20),
+                  ),
+                  SizedBox(height: 63),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: Size(281, 48),
+                        primary: Color.fromRGBO(86, 151, 211, 1),
+                        backgroundColor: Colors.white,
+                        shadowColor: Colors.black54,
+                        elevation: 10,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        textStyle: TextStyle(
+                            fontSize: 18, fontFamily: 'HammersmithOne'),
+                      ),
+                      child: Text('Log In'),
+                      onPressed: () async {
+                        Navigator.of(context)
+                            .popUntil((route) => route.isFirst);
+                      })
+                ]))));
   }
 }
