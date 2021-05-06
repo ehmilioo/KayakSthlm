f61a2af6d2316b5f4f4d04eb872fdd9b460b9a14
Lindn1 <83230921+Lindn1@users.noreply.github.com>
2021-05-06 17:02:17 +0200

    update
    update

--- kayak_sthlm/lib/screens/info/guides.dart
+++ kayak_sthlm/lib/screens/info/guides.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'information.dart';
 
 class GuideScreen extends StatelessWidget {
