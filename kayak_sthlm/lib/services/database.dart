a716a30cc83174b2b7446964d8e48e94d0923b24
emilkrslak <krslak_emil@hotmail.com>
2021-05-12 12:46:42 +0200

    Update database.dart
    Cleanup

--- kayak_sthlm/lib/services/database.dart
+++ kayak_sthlm/lib/services/database.dart
@@ -4,25 +4,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Database {
   String uid = FirebaseAuth.instance.currentUser.uid;
 
-  void deleteUser(){
-    print('lol');
-  }
-
-  void saveUserSettings(){
-    print('saved user');
-  }
-
-  void changePassword(){
-    print('changed password');
-  }
-
   Future<void> updateUser(String username, String age, String experience, String gender) {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
   return users
     .doc(uid)
     .update({'age': age, 'username': username, 'experience': experience, 'gender': gender})
     .then((value) => print("User updated"))
-    .catchError((error) => print("Failed to update user's property: $error"));
+    .catchError((error) => print("Failed: $error"));
 }
 
 
