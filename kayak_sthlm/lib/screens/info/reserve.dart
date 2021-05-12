634cb5bee75d3c6e608606c4e0652329cd273d3a
Androsnian <83273759+Androsnian@users.noreply.github.com>
2021-05-12 10:15:52 +0200

    Finputsade Rules på Information -> Map of Reserves. Arbetade på API, men verkar inte bli rätt ännu.

--- kayak_sthlm/lib/screens/info/reserve.dart
+++ kayak_sthlm/lib/screens/info/reserve.dart
@@ -7,127 +7,71 @@ class ReserveScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Map of Reserves'),
-      ),
-      body: Center(
+        appBar: AppBar(
+          title: Text('Map of Reserves'),
+        ),
+        body: Center(
           child: Column(
             children: <Widget>[
-              BottomAppBar(
-                child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: ElevatedButton(
-                    child: Text('Rules when moving in nature'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ReserveRulesScreen()));
-                    },
-                  ),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xff7c94b6),
-                  image: const DecorationImage(
-                    image: AssetImage('asset/images/mapNatureReserves.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 8,
-                  ),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-        children: <Widget>[
           BottomAppBar(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: ElevatedButton(
-                child: Text('Rules'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ReserveRulesScreen()));
-                },
-              ),
+          child: Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: ElevatedButton(
+              child: Text('Rules when moving in nature'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ReserveRulesScreen()));
+              },
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              return showDialog(
-                context: context,
-                builder: (ctx) => AlertDialog(
-                  title: Text("Rules"),
-                  content: Text(
-                      "Avoid plants and trees. You are not allowed to pick protected plants; for instance orchids."
-                      "Avoid bird nests and their children. Do not hurt snakes, reptiles and frogs."
-                      "Avoid birds and seal sanctuaries when the access is prohibited."
-                      "Your dog needs to be tethered at all time."
-                      "Do not start a fire. It is also forbidden to put disposable grills in the garbage!",
-                      style: TextStyle(fontSize: 20)),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(ctx).pop();
-                      },
-                      child: Text("Close"),
-                    ),
-                  ],
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: const DecorationImage(
+              image: AssetImage('asset/images/mapNatureReserves.png'),
+              fit: BoxFit.cover,
+            ),
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                image: const DecorationImage(
+                  image: AssetImage('asset/images/mapNatureReserves.png'),
+                  fit: BoxFit.cover,
                 ),
-              );
-            },
-            child: Text("Show Rules"),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: AssetImage('asset/images/mapNatureReserves.png'),
-                fit: BoxFit.cover,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 8,
+                ),
+                borderRadius: BorderRadius.circular(12),
               ),
-              border: Border.all(
-                color: Colors.black,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
             ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: ElevatedButton(
-                child: Text('Back'),
-                onPressed: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => InformationScreen()));
-                },
-              ),
-              Expanded(
+            Expanded(
                 child: Align(
                   alignment: FractionalOffset.bottomCenter,
                   child: ElevatedButton(
                     child: Text('Back'),
                     onPressed: () {
-                      Navigator.push(
+                      Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
                               builder: (context) => InformationScreen()));
                     },
                   ),
-                ),
-              ),
-    ),
-    )
-    ]
-          )],
-    );
-    }
-
+                )
+            )],
+        )
+    ));
+  }
 }
 
 class ReserveRulesScreen extends StatelessWidget {
@@ -166,34 +110,6 @@ class ReserveRulesScreen extends StatelessWidget {
               )
             ],
           )),
-        children: <Widget>[
-          Text(
-              "Avoid plants and trees. You are not allowed to pick protected plants;for instance orchids.",
-              style: TextStyle(fontSize: 20)),
-          Text(
-              "Avoid bird nests and their children. Do not hurt snakes, reptiles and frogs. ",
-              style: TextStyle(fontSize: 20)),
-          Text(
-              "Avoid birds and seal sancutaries when the access is prohibited.",
-              style: TextStyle(fontSize: 20)),
-          Text("Your dog needs to be tethered at all time. ",
-              style: TextStyle(fontSize: 20)),
-          Text(
-              "Do not start a fire. It is also forbidden to put disposable grills in the garbage!",
-              style: TextStyle(fontSize: 20)),
-          Expanded(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: ElevatedButton(
-                child: Text('Back'),
-                onPressed: () {
-                  Navigator.pop(context,
-                      MaterialPageRoute(builder: (context) => ReserveScreen()));
-                },
-              ),
-            ),
-          )
-        ],
-      );
+    );
   }
 }
