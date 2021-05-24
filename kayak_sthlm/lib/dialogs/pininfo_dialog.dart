174f3a5d05ccf34a94ca56f02ac187ac644d094f
emilkrslak <krslak_emil@hotmail.com>
2021-05-24 21:53:49 +0200

    Avslutade pins

--- kayak_sthlm/lib/dialogs/pininfo_dialog.dart
+++ kayak_sthlm/lib/dialogs/pininfo_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
@@ -15,6 +17,26 @@ class _PinInfo extends State<PinInfo> {
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<Map<String, dynamic>> getDetails() async {
+    String _url = 'maps.googleapis.com';
+    Map<String, String> _parameters = {
+      'key':
+          'AIzaSyAQB7qDxdH_-5bpTA6IvQjlPru7fkOPqQY', //Denna ska egentligen gömmas med dotenv..
+      'place_id': widget.item['place_id'],
+      'fields':
+          'website,rating,opening_hours/weekday_text,geometry/location,name,rating,formatted_phone_number',
+    };
+    String _path = '/maps/api/place/details/json';
+    final response = await http.get(Uri.https(_url, _path, _parameters));
+    final Map<String, dynamic> data = json.decode(response.body);
+    if (response.statusCode == 200) {
+      return data;
+    } else {
+      print('Failed to load API ${response.statusCode} ');
+      return null;
+    }
   }
 
   @override
@@ -84,48 +106,88 @@ class _PinInfo extends State<PinInfo> {
               ],
             ),
           )
-        : Card(
-            color: Colors.transparent,
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(
-                      left: 20, top: 120, right: 20, bottom: 20),
-                  margin: EdgeInsets.only(top: 45),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black,
-                            offset: Offset(0, 10),
-                            blurRadius: 10),
-                      ]),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
+        : FutureBuilder(
+            future: getDetails(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var resData = snapshot.data['result'];
+                return Card(
+                  color: Colors.transparent,
+                  child: Stack(
                     children: <Widget>[
-                      Text(
-                        'Confirmation',
-                        style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w200,
-                            fontFamily: 'HammersmithOne'),
+                      Container(
+                        padding: EdgeInsets.only(
+                            left: 20, top: 120, right: 20, bottom: 20),
+                        margin: EdgeInsets.only(top: 45),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black,
+                                  offset: Offset(0, 10),
+                                  blurRadius: 10),
+                            ]),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            IconButton(
+                              color: Colors.black,
+                              icon: Icon(Icons.close_outlined),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                            resData['name'] != null
+                                ? Text(
+                                    resData['name'],
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w200,
+                                        fontFamily: 'HammersmithOne'),
+                                  )
+                                : Text('No name is specified'),
+                            SizedBox(
+                              height: 22,
+                            ),
+                            resData['formatted_phone_number'] != null
+                                ? Text(
+                                    'Number: ${resData['formatted_phone_number']}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w200,
+                                      fontFamily: 'HammersmithOne',
+                                    ))
+                                : Text(
+                                    'No phone number is specified by owners'),
+                            resData['website'] != null
+                                ? Text('Website: ${resData['website']}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w200,
+                                      fontFamily: 'HammersmithOne',
+                                    ))
+                                : Text('No website is specified by owners'),
+                            resData['opening_hours'] != null
+                                ? Text(
+                                    '${resData['opening_hours']['weekday_text'][0]} \n ${resData['opening_hours']['weekday_text'][1]} \n ${resData['opening_hours']['weekday_text'][2]} \n ${resData['opening_hours']['weekday_text'][3]} \n ${resData['opening_hours']['weekday_text'][4]} \n ${resData['opening_hours']['weekday_text'][5]} \n ${resData['opening_hours']['weekday_text'][6]} \n',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w200,
+                                      fontFamily: 'HammersmithOne',
+                                    ))
+                                : Text(
+                                    'No opening hours is specified by owners'),
+                          ],
+                        ),
                       ),
-                      SizedBox(
-                        height: 22,
-                      ),
-                      Text('Inte Custom Pin',
-                          style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w200,
-                              fontFamily: 'HammersmithOne',
-                              color: Colors.red[800])),
                     ],
                   ),
-                ),
-              ],
-            ),
-          );
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            });
   }
 }
--- kayak_sthlm/lib/dialogs/weather_dialog.dart
+++ kayak_sthlm/lib/dialogs/weather_dialog.dart
@@ -8,10 +8,7 @@ class WeatherDialog extends StatefulWidget {
   final double longitude;
   final double latitude;
 
-  WeatherDialog({
-    @required this.longitude, 
-    @required this.latitude
-  });
+  WeatherDialog({@required this.longitude, @required this.latitude});
 
   @override
   State<StatefulWidget> createState() => WeatherOverlayState();
@@ -28,10 +25,13 @@ class WeatherOverlayState extends State<WeatherDialog> {
   }
 
   Future<Weather> fetchWeather() async {
-    String userLongitude = widget.longitude.toStringAsFixed(6).toString(); //Korta ner stringsen eftersom SMHI tar odokumenterat in endast 6 decimaler.
+    String userLongitude = widget.longitude
+        .toStringAsFixed(6)
+        .toString(); //Korta ner stringsen eftersom SMHI tar odokumenterat in endast 6 decimaler.
     String userLatitude = widget.latitude.toStringAsFixed(6).toString();
     String url = "opendata-download-metfcst.smhi.se";
-    final response = await http.get(Uri.https(url, '/api/category/pmp3g/version/2/geotype/point/lon/$userLongitude/lat/$userLatitude/data.json'));
+    final response = await http.get(Uri.https(url,
+        '/api/category/pmp3g/version/2/geotype/point/lon/$userLongitude/lat/$userLatitude/data.json'));
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
     } else {
@@ -40,26 +40,21 @@ class WeatherOverlayState extends State<WeatherDialog> {
     }
   }
 
-  
-
   // Fetch content from the json file
   Future<void> readJson() async {
-    final String response = await DefaultAssetBundle.of(context).loadString('assets/weatherType.json');
+    final String response = await DefaultAssetBundle.of(context)
+        .loadString('assets/weatherType.json');
     final data = await json.decode(response);
-    setState(() => {
-      _types = data['types']
-    });
+    setState(() => {_types = data['types']});
   }
 
-  Map<String, dynamic> getWeatherTypeInfo(int index){
-    if(_types != null){
+  Map<String, dynamic> getWeatherTypeInfo(int index) {
+    if (_types != null) {
       return _types[index];
-    }else{
+    } else {
       return null;
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -67,41 +62,73 @@ class WeatherOverlayState extends State<WeatherDialog> {
       future: currentWeather,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          Map<String, dynamic> response = getWeatherTypeInfo(snapshot.data.timeSeries[0]['parameters'][18]['values'][0]-1); //-1 eftersom SMHI API börjar på 1 och listor på 0 :-)
+          Map<String, dynamic> response = getWeatherTypeInfo(
+              snapshot.data.timeSeries[0]['parameters'][18]['values'][0] -
+                  1); //-1 eftersom SMHI API börjar på 1 och listor på 0 :-)
           return Stack(
             children: <Widget>[
               Container(
-                padding: EdgeInsets.only(left: Constants.padding,top: 120, right: Constants.padding,bottom: Constants.padding
-                ),
+                padding: EdgeInsets.only(
+                    left: Constants.padding,
+                    top: 120,
+                    right: Constants.padding,
+                    bottom: Constants.padding),
                 margin: EdgeInsets.only(top: Constants.avatarRadius),
                 decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(Constants.padding),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black,offset: Offset(0,10),
-                    blurRadius: 10
-                    ),
-                  ]
-                ),
+                    shape: BoxShape.rectangle,
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(Constants.padding),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(0, 10),
+                          blurRadius: 10),
+                    ]),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Text('Temp: ${snapshot.data.timeSeries[0]['parameters'][10]['level']} °C',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
-                    SizedBox(height: 15,),
-                    Text('Wind: ${snapshot.data.timeSeries[0]['parameters'][14]['level']} m/s',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
-                    SizedBox(height: 15,),
-                    Text('Weather type: ${response['desc']}',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
-                    SizedBox(height: 15,),
-                    Text('Rain: ${snapshot.data.timeSeries[0]['parameters'][0]['level']}%',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
-                    SizedBox(height: 22,),
+                    Text(
+                      'Temp: ${snapshot.data.timeSeries[0]['parameters'][10]['level']} °C',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Wind: ${snapshot.data.timeSeries[0]['parameters'][14]['level']} m/s',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Weather type: ${response['desc']}',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Rain: ${snapshot.data.timeSeries[0]['parameters'][0]['level']}%',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
                     Align(
                       alignment: Alignment.bottomRight,
                       child: FlatButton(
-                          onPressed: (){
+                          onPressed: () {
                             Navigator.of(context).pop();
                           },
-                          child: Text('Close',style: TextStyle(fontSize: 18),)),
+                          child: Text(
+                            'Close',
+                            style: TextStyle(fontSize: 18),
+                          )),
                     ),
                   ],
                 ),
@@ -123,7 +150,7 @@ class WeatherOverlayState extends State<WeatherDialog> {
 //  child: Text(snapshot.data.timeSeries[0]['parameters'][10]['name']),
 //  https://opendata.smhi.se/apidocs/metfcst/parameters.html
 //  timeSeries[0]['parameters'][10]['name']
-//  Lista    Timme            Format Värde  
+//  Lista    Timme            Format Värde
 
 class Weather {
   final List timeSeries;
@@ -141,8 +168,8 @@ class Weather {
   }
 }
 
-class Constants{
+class Constants {
   Constants._();
-  static const double padding =20;
-  static const double avatarRadius =45;
+  static const double padding = 20;
+  static const double avatarRadius = 45;
 }
